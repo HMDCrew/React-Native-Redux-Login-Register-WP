36352793e6c0b21c05932dc42cefa5e84ffdaf28
Andrei L <andrei.leca55@gmail.com>
2022-09-16 11:24:10 +0200

    Add AsyncStorage

--- store/REST/api.js
+++ store/REST/api.js
@@ -1,12 +1,24 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import env from '../../constants/env'
+import AsyncStorage from '@react-native-async-storage/async-storage' 
 import axios from 'axios'
 
+import env from '../../constants/env'
 
 export const getLoginToken = createAsyncThunk('store/login', async ({ user, passwd }) => {
-    return await axios.post(env.SITE_URL + "wp-json/jwt-auth/v1/token", {
-        username: user,
-        password: passwd
-    }).then((response) => response.data )
-    .catch((error) => error.json() );
+    
+    const login_data = await AsyncStorage.getItem('login-data');
+
+    if (null !== login_data) {
+
+        return JSON.parse(login_data);
+
+    } else {
+        return await axios.post(env.SITE_URL + "wp-json/jwt-auth/v1/token", {
+            username: user,
+            password: passwd
+        }).then((response) => {
+            AsyncStorage.setItem('login-data', JSON.stringify(response.data))
+            return response.data
+        })
+    }
 });
